cf0e7c20aa814f2a10c8c9415df832b49156e83b
vaibhavambavkar1 <36914381+vaibhavambavkar1@users.noreply.github.com>
2025-11-21 15:57:16 +0530

    Update setup.py

--- django-daisyui-vaibhav-tailwind/setup.py
+++ django-daisyui-vaibhav-tailwind/setup.py
@@ -13,9 +13,6 @@ setup(
     description='A modern django dashboard built with daisyui',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/hypy13/django-daisy',
-    author='Hossein Yaghoubi',
-    author_email='hossein.yaghoubi13@gmail.com',
     license='Apache 2.0',
     classifiers=[
         'Programming Language :: Python :: 3',
