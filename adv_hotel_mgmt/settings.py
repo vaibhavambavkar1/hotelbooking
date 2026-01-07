38d6e4047164284eac0229c6bffed79b88953229
krunal <krunalkadam0703@gmail.com>
2026-01-07 19:50:06 +0000

    docker setup

--- adv_hotel_mgmt/settings.py
+++ adv_hotel_mgmt/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2d0!4!g1cd^nk%lz)@@7-wqz&30_neu1oy0w0nqdzf-d!-=fkk'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", 
+    "django-insecure-2d0!4!g1cd^nk%lz)@@7-wqz&30_neu1oy0w0nqdzf-d!-=fkk"
+)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
@@ -85,11 +88,14 @@ WSGI_APPLICATION = 'adv_hotel_mgmt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("POSTGRES_DB", "hotelbooking_db"),
+        'USER': os.environ.get("POSTGRES_USER", "hotel_user"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD", "StrongPassword@123"),
+        'HOST': os.environ.get("POSTGRES_HOST", "db"),
+        'PORT': os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
